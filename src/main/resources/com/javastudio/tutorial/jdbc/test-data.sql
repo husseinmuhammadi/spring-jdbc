f58d2dd732c8e6f644589495f90529b5f9394661
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-10-12 20:31:46 +0330

    Spring Jdbc

---
+++ src/main/resources/com/javastudio/tutorial/jdbc/test-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO CUSTOMER VALUES (1, 'James', 'Gosling', 'Canada');
+
+INSERT INTO CUSTOMER VALUES (2, 'Donald', 'Knuth', 'USA');
+
+INSERT INTO CUSTOMER VALUES (3, 'Linus', 'Torvalds', 'Finland');
+
+INSERT INTO CUSTOMER VALUES (4, 'Dennis', 'Ritchie', 'USA');
--- src/test/java/com/javastudio/tutorial/jdbc/CustomerRepositoryTest.java
+++ src/test/java/com/javastudio/tutorial/jdbc/CustomerRepositoryTest.java
@@ -1,32 +1,27 @@
 package com.javastudio.tutorial.jdbc;
 
 import com.javastudio.tutorial.jdbc.dao.CustomerDao;
-import com.javastudio.tutorial.jdbc.model.Customer;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 
 @JdbcTest
+@Sql({"schema.sql", "test-data.sql"})
 public class CustomerRepositoryTest {
     private final Logger logger = LoggerFactory.getLogger(CustomerRepositoryTest.class);
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @BeforeEach
-    void setUp() {
-        jdbcTemplate.update("INSERT INTO customer(first_name, last_name) values (?, ?)", "Hossein", "Mohammadi");
-    }
-
     @Test
     void whenAddCustomer_thenItMustPersist() {
         CustomerDao customerDao = new CustomerDao(jdbcTemplate);
-        logger.info("{}", customerDao.getCountOfCustomers());
-        logger.info("_________________________");
+        int countOfCustomers = customerDao.getCountOfCustomers();
+        Assertions.assertThat(countOfCustomers).isEqualTo(4);
     }
-
 }
